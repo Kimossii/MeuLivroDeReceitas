f4c63d0e471b375bff902ead8f1189c85860b254
eluck <eluckimossi@gmail.com>
2026-05-22 09:01:22 +0200

    feat: adicionando endpoint for register user account

---
+++ src/Shared/MyRecepeBook.Communication/Requests/RequestRegisterUserAccountJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecepeBook.Communication.Requests;
+
+public class RequestRegisterUserAccountJson
+{
+    public string Name { get; set; } = String.Empty;
+    public string Email { get; set; } = String.Empty;
+    public string Password { get; set; } = String.Empty;
+}
